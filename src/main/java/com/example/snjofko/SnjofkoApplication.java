9139f8634bf0ee8eb06d34564a34fa58d5d41b80
Kristijan <kristijan.milanovic1@gmail.com>
2022-07-18 16:27:06 +0200

    Initial commit

---
+++ src/main/java/com/example/snjofko/SnjofkoApplication.java
@@ -0,0 +1,13 @@
+package com.example.snjofko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnjofkoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnjofkoApplication.class, args);
+	}
+
+}
